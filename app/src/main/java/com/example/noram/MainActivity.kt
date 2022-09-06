2af6492a7421a829883296ec0f84b20cf0eab8d9
NOAH <noahmuhindi11@gmail.com>
2022-09-06 15:17:40 +0100

    Login-UI

---
+++ app/src/main/java/com/example/noram/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.noram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.noram.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+   binding.button.setOnClickListener {
+    binding.button.text = "chat"
+}
+
+
+    }
+}
